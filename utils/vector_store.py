b2922a894e1b4b3b6c80127c649bab35bf5be5fb
Vendra Lakshmi Saranya <saranyalakshmi641@gmail.com>
2026-05-28 11:13:59 +0530

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import FAISS
+'''from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
@@ -7,6 +7,42 @@ def create_vector_store(text_chunks):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
+
+    vectorstore = FAISS.from_texts(
+        text_chunks,
+        embedding=embeddings
+    )
+
+    return vectorstore'''
+
+
+from langchain_community.vectorstores import FAISS
+
+from langchain_community.embeddings import (
+    HuggingFaceEmbeddings
+)
+
+import os
+
+
+def create_vector_store(text_chunks):
+
+    # Create cache folder manually
+
+    cache_folder = "hf_cache"
+
+    os.makedirs(cache_folder, exist_ok=True)
+
+    # Load embeddings model
+
+    embeddings = HuggingFaceEmbeddings(
+
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+
+        cache_folder=cache_folder
+    )
+
+    # Create FAISS vector store
 
     vectorstore = FAISS.from_texts(
         text_chunks,
